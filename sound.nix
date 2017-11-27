2de66df1efef543315ae33061572073c4cbd053c
Your Name <you@example.com>
2017-11-27 00:58:57 -0300

    add ladspa to mplayer

--- sound.nix
+++ sound.nix
@@ -64,6 +64,9 @@ in {
 
   environment.systemPackages = [
     pkgs.jack2Full
+    pkgs.ladspaPlugins
+    pkgs.ladspa-sdk
+    # pkgs.renoise
   ];
 
   sound = {
@@ -72,8 +75,46 @@ in {
       defaults.pcm.!cart   PCH
       defaults.pcm.!device 0
       defaults.pcm.!ctl    PCH
+
+      pcm.mplayer{
+        type plug
+        slave.pcm "mplayer_limiter";
+      }
+      pcm.mplayer_limiter{
+        type ladspa
+        path "${pkgs.ladspaPlugins}/lib/ladspa";
+        slave.pcm default
+        plugins [
+          { label dysonCompress
+              input {
+                controls [
+                  0   # peak limit
+                  2   # release time
+                  0.2 # fast ratio
+                  0.8 # ratio
+                ]
+              }
+          }
+          { label fastLookaheadLimiter
+            input {
+              controls [
+                30  # InputGain(Db) -20 -> +20
+                -10 # Limit (db) -20 -> 0
+                1.1 # Release time (s) 0.01 -> 2
+              ]
+            }
+          }
+        ]
+      }
+
       '';
   };
+
+  programs.bash.interactiveShellInit = ''
+    # set ladspa library path
+    # about testing the plugins check analyseplugin command
+    export LADSPA_PATH=${pkgs.ladspaPlugins}/lib/ladspa
+  '';
 
   # todo : this is not working yet
   #imports = [
