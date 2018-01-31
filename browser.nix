c6584e96b7a803cebf936f6914610f9da8bd13cc
Your Name <you@example.com>
2018-02-01 08:57:08 +1300

    changed browser using firejail and add desktop files

--- browser.nix
+++ browser.nix
@@ -1,144 +1,140 @@
 { config, lib, pkgs, ... }:
 
-# browser
-# -------
-# Don't forget to run 'xhost +' with your user
-# to make sure the browser user can write to X
 let
 
-  # browser select
-  # --------------
-  browser-select = pkgs.writeScriptBin "browser-select" chrome-select-text;
-  browser_paths  = chrome-paths;
-  browser_case   = chrome-case;
 
-  # google-chrome
-  # -------------
-  google-chrome-select = pkgs.writeScriptBin "google-chrome-select" google-chrome-select-text;
-  google-chrome-select-text = ''
-    BROWSER=$(echo -e "${lib.concatStringsSep "\\n" google-chrome-paths}" | ${pkgs.dmenu}/bin/dmenu)
-    case $BROWSER in
-    ${lib.concatMapStringsSep "\n" google-chrome-case google-chrome-paths}
-    esac
-    $BIN "$@"
-  '';
-  google-chrome-paths = builtins.attrNames config.google-chrome.paths;
-  google-chrome-case = n: ''
-    ${n}) export BIN=${config.google-chrome.paths.${n}}/bin/${n}
-            ;;
+  # desktop file
+  # ------------
+  # makes it possible to be used by other programs
+  desktop-file = browser-name: bin: pkgs.writeTextFile {
+    name        = "${browser-name}.desktop" ;
+    destination = "/share/applications/${browser-name}.desktop";
+    text        = ''
+      [Desktop Entry]
+      Type=Application
+      Exec=${bin}/bin/${browser-name} %U
+      Icon=chromium
+      Comment=An open source web browser from Google
+      Terminal=false
+      Name=${browser-name}
+      GenericName=Web browser
+      MimeType=text/html;text/xml;application/xhtml+xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/mailto;x-scheme-handler/webcal;x-scheme-handler/about;x-sc
+      Categories=Network;WebBrowser
+      StartupWMClass=${browser-name}
     '';
+  };
 
-  # chromium
+
+  # browsers
   # --------
-  chrome-select = pkgs.writeScriptBin "chrome-select" chrome-select-text;
-  chrome-select-text = ''
-    BROWSER=$(echo -e "${lib.concatStringsSep "\\n" chrome-paths}" | ${pkgs.dmenu}/bin/dmenu)
-    case $BROWSER in
-    ${lib.concatMapStringsSep "\n" chrome-case chrome-paths}
-    esac
-    $BIN "$@"
-  '';
-  chrome-paths = builtins.attrNames config.chrome.paths;
-  chrome-case = n: ''
-    ${n}) export BIN=${config.chrome.paths.${n}}/bin/${n}
-            ;;
-    '';
+  # to be choosen by the createBrowser function
+  google-browser   = "${pkgs.google-chrome}/bin/google-chrome-stable $@";
+  chromium-browser = "${pkgs.chromium}/bin/chromium $@";
+  firefox-browser  = "${pkgs.firefox}/bin/firefox $@";
 
-  # firefox
-  # -------
-  firefox-select = pkgs.writeScriptBin "firefox-select" firefox-select-text;
-  firefox-select-text = ''
-    BROWSER=$(echo -e "${lib.concatStringsSep "\\n" firefox-paths}" | ${pkgs.dmenu}/bin/dmenu)
-    case $BROWSER in
-    ${lib.concatMapStringsSep "\n" firefox-case firefox-paths}
-    esac
-    $BIN "$@"
-  '';
-  firefox-paths = builtins.attrNames config.firefox.paths;
-  firefox-case = n: ''
-    ${n}) export BIN=${config.firefox.paths.${n}}/bin/${n}
-            ;;
-    '';
-
-  # create browser
-  # --------------
-  createBrowser = name: groups:
+  # creates a jailed browser
+  # ------------------------
+  createBrowser = browser-name: browser: home-folder:
     let
+      bin = pkgs.writeShellScriptBin "${browser-name}" ''
+        if [[ ! -d ${home-folder} ]]
+        then
+          mkdir -p ${home-folder}
+        fi
 
-      google-chrome-bin = pkgs.writeShellScriptBin "google-chrome-${name}" ''
-        /var/run/wrappers/bin/sudo -u browser-${name} -i ${pkgs.google-chrome}/bin/google-chrome-stable $@
+        /var/run/wrappers/bin/firejail \
+          --private=${home-folder} \
+          ${browser}
       '';
-      chrome-bin = pkgs.writeShellScriptBin "chrome-${name}" ''
-        /var/run/wrappers/bin/sudo -u browser-${name} -i ${pkgs.chromium}/bin/chromium $@
-      '';
-      firefox-bin = pkgs.writeShellScriptBin "firefox-${name}" ''
-        /var/run/wrappers/bin/sudo -u browser-${name} -i ${pkgs.firefox}/bin/firefox $@
-      '';
-
     in {
 
-      users.users."browser-${name}" = {
-        isNormalUser = true;
-        home = "/home/browser-${name}";
-        description = "A servant who opens the browser for me";
-        extraGroups = groups;
-        createHome = true;
-      };
+      # add to browser-select
+      # ---------------------
+      browser.list ."${browser-name}" = bin ;
 
-      google-chrome.paths."google-chrome-${name}" = google-chrome-bin;
-      chrome.paths."chrome-${name}" = chrome-bin;
-      firefox.paths."firefox-${name}" = firefox-bin;
-
-      security.sudo.extraConfig = ''
-        palo ALL=(browser-${name}) NOPASSWD: ALL
-      '';
-
+      # install
+      # -------
       environment.systemPackages = [
-        google-chrome-bin
-        chrome-bin
-        firefox-bin
-        pkgs.xorg.xhost
+        bin
+        ( desktop-file browser-name bin )
+        pkgs.firejail
       ];
-
     };
+
+  createBrowserInHome = browser-name: browser:
+    createBrowser browser-name browser "~/.firejail-${browser-name}";
+
+  # browser chooser
+  # ---------------
+  browser-select = pkgs.writeScriptBin  "browser-select" ''
+
+    # select a browser using dmenu
+    # ----------------------------
+    BROWSER=$( echo -e "${lib.concatStringsSep "\\n" browser-list}" \
+      | ${pkgs.dmenu}/bin/dmenu )
+
+    # start selected browser
+    # ----------------------
+    case $BROWSER in
+    ${lib.concatMapStringsSep "\n" browser-case browser-list}
+    esac
+    $BIN "$@"
+  '';
+  browser-list = builtins.attrNames config.browser.list;
+  browser-case  = bin: ''
+    ${bin}) export BIN=${config.browser.list.${bin}}/bin/${bin}
+    ;;
+  '';
 
 in {
 
-  # setup browser-select
-  # --------------------
-  options.browser.paths = lib.mkOption {
-    type = with lib.types;
-    attrsOf path;
-  };
-  options.firefox.paths = lib.mkOption {
-    type = with lib.types;
-    attrsOf path;
-  };
-  options.chrome.paths = lib.mkOption {
-    type = with lib.types;
-    attrsOf path;
-  };
-  options.google-chrome.paths = lib.mkOption {
+  # selectable browsers
+  # -------------------
+  # is filled in the createBrowser function
+  # and collected in the browser-select script
+  options.browser.list = lib.mkOption {
     type = with lib.types;
     attrsOf path;
   };
   config.environment.systemPackages = [
     browser-select
-    chrome-select
-    google-chrome-select
-    firefox-select
+    ( desktop-file browser-select.name browser-select )
   ];
 
-  # create all kinds of browsers
-  # ----------------------------
+  # add browser
+  # -----------
   imports = [
-    ( createBrowser "sononym"     [ "audio" ] )
-    ( createBrowser "facebook"    [ "audio" ] )
-    ( createBrowser "development" [ "audio" ] )
-    ( createBrowser "tmp"         [ "audio" ] )
-    ( createBrowser "hangout"     [ "audio" "video" ] )
-    ( createBrowser "finance"     [ ] )
+
+    # top specific browser
+    # --------------------
+    ( createBrowserInHome "finance"     firefox-browser )
+    ( createBrowserInHome "facebook"    chromium-browser )
+    ( createBrowserInHome "development" chromium-browser )
+    ( createBrowserInHome "hangout"     google-browser )
+
+    # RAM browser
+    # -----------
+    ( createBrowser "firefox-tmp"  firefox-browser  "/dev/shm/firefox-tmp/" )
+    ( createBrowser "chromium-tmp" chromium-browser "/dev/shm/chromium-tmp/" )
+    ( createBrowser "google-tmp"   google-browser   "/dev/shm/google-tmp/" )
+
   ];
 
+
+  # set the default browser system-wide
+  # -----------------------------------
+  # check with : xdg-mime query default text/html
+  # todo : this should be configurable by a module
+  # todo : not working right now : current fix :
+  #        xdg-settings set default-web-browser browser-select.desktop
+
+  config.environment.etc."xdg/mimeapps.list".source = pkgs.writeText "mimeapps.list" ''
+    [Default Applications]
+    text/html=${browser-select.name}.desktop
+    x-scheme-handler/http=${browser-select.name}.desktop
+    x-scheme-handler/https=${browser-select.name}.desktop
+    x-scheme-handler/about=${browser-select.name}.desktop
+    x-scheme-handler/unknown=${browser-select.name}.desktop
+  '';
 
 }
